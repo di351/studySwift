fe7cb2f8803f4ad687105fc79ca64de7f9b3d8a5
di351 <di35180@gmail.com>
2024-06-24 13:00:00 +0900

    feat: let, var

---
+++ studySwift/studySwift/01/code.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+let constant: String = "변경이 불가능한 상수 let"
+var variable: String = "변경이 가능한 변수 var"
+
+variable = "변수는 다른 값을 할당 할 수 있지만"
+// constant = "상수는 다른 값을 할당 할 수 없음"
+
+let sum: Int
+let inputA: Int = 100
+let inputB: Int = 200
+
+sum = inputA + inputB
+
+var nickName: String
+
+nickName = "dakeong"
+
+nickName = "다경"
