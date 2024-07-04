d567ae6bc2f9117b5a3f28f787decb40ee2474e9
di351 <di35180@gmail.com>
2024-07-04 13:00:00 +0900

    feat: class

---
+++ studySwift/studySwift/09/code.playground/Contents.swift
@@ -0,0 +1,64 @@
+import UIKit
+
+class Sample {
+    var mutableProperty: Int = 100
+    
+    let immutableProperty: Int = 100
+    
+    @MainActor static var typeProperty: Int = 100
+    
+    func instanceMethod() {
+        print("instance method")
+    }
+    
+    static func typeMethod() {
+        print("type method - static")
+    }
+    
+    class func classMethod() {
+        print("type method - class")
+    }
+}
+
+
+var mutableReference: Sample = Sample()
+
+mutableReference.mutableProperty = 200
+
+let immutableReference: Sample = Sample()
+
+immutableReference.mutableProperty = 200
+
+//immutableReference = mutableReference
+
+//immutableReference.immutableProperty = 200
+
+
+Sample.typeProperty = 300
+Sample.typeMethod()
+
+
+class Student {
+    var name: String = "unknown"
+    
+    var `class`: String = "Swift"
+
+    class func selfIntroduce() {
+        print("학생타입입니다")
+    }
+    
+    func selfIntroduce() {
+        print("저는 \(self.class)반 \(name)입니다")
+    }
+}
+
+Student.selfIntroduce()
+
+var yagom: Student = Student()
+yagom.name = "dakeong"
+yagom.class = "스위프트"
+yagom.selfIntroduce()
+
+let jina: Student = Student()
+jina.name = "jina"
+jina.selfIntroduce()
