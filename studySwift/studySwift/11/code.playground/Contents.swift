17203651bbeab48b2e1c3c8a062d6bedfb85704d
di351 <di35180@gmail.com>
2024-07-06 14:00:00 +0900

    feat: enum

---
+++ studySwift/studySwift/11/code.playground/Contents.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+enum Weekday {
+    case mon
+    case tue
+    case wed
+    case thu, fri, sat, sun
+}
+
+var day: Weekday = Weekday.mon
+day = .tue
+
+print(day)
+
+switch day {
+case .mon, .tue, .wed, .thu:
+    print("평일입니다")
+case Weekday.fri:
+    print("불금 파티!!")
+case .sat, .sun:
+    print("신나는 주말!!")
+}
+
+enum Fruit: Int {
+    case apple = 0
+    case grape = 1
+    case peach
+//    case mango = 0
+}
+
+print("Fruit.peach.rawValue == \(Fruit.peach.rawValue)")
+// Fruit.peach.rawValue == 2
+
+enum School: String {
+    case elementary = "초등"
+    case middle = "중등"
+    case high = "고등"
+    case university
+}
+
+print("School.middle.rawValue == \(School.middle.rawValue)")
+
+print("School.university.rawValue == \(School.university.rawValue)")
+
+//let apple: Fruit = Fruit(rawValue: 0)
+let apple: Fruit? = Fruit(rawValue: 0)
+
+if let orange: Fruit = Fruit(rawValue: 5) {
+    print("rawValue 5에 해당하는 케이스는 \(orange)입니다")
+} else {
+    print("rawValue 5에 해당하는 케이스가 없습니다")
+}
+
+enum Month {
+    case dec, jan, feb
+    case mar, apr, may
+    case jun, jul, aug
+    case sep, oct, nov
+    
+    func printMessage() {
+        switch self {
+        case .mar, .apr, .may:
+            print("따스한 봄~")
+        case .jun, .jul, .aug:
+            print("여름 더워요~")
+        case .sep, .oct, .nov:
+            print("가을은 독서의 계절!")
+        case .dec, .jan, .feb:
+            print("추운 겨울입니다")
+        }
+    }
+}
+
+Month.mar.printMessage()
